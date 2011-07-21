9f4b6cc4a09dd02e552c96793405314798f18ade
juannazaro@gmail.com <juannazaro@gmail.com@681c648b-e3cb-439b-8fd7-2cea78f62630>
2011-07-21 15:34:22 +0000

    Cambios significativos en la clase Cmdsphere

--- Minecraft/src/craftforfood/myessentials/commands/Cmdsphere.java
+++ Minecraft/src/craftforfood/myessentials/commands/Cmdsphere.java
@@ -1,58 +1,91 @@
-package craftforfood.myessentials.commands;
+package crash.myessentials.commands;
 
-import org.bukkit.block.Block;
-
+import org.bukkit.block; 
 import java.lang.Math;
 import java.util.Stack;
 
 public class Cmdsphere extends MyECommand {
-		private double mCubes; //maximum number of cubes
-		private double radius; //radius of the sphere
-		private Stack slices;  //number of slices to build the sphere
-		private Block b;
-		private int r;
-		private int c; 
-		private int id;
+		private double mCubes; 	//maximum number of cubes
+		private double radius; 	//radius of the sphere
+		private Stack slices;  	//number of slices to build the sphere
+		private Block b;		//block that contains the axis, where will be placed  the sphere
+		private int r;			//variable to store the radius of the sphere to create
+		private int c; 			//variable to store if the sphere is centered or not
+		private int idI;		//variable to store the material inside the sphere
+		private int idS;		//variable to store the material of the sphere
+		private int idO;		//variable to store the material the sphere
 		
-        public Cmdsphere() {
-        	super("build.sphere");
-        	
+		
+		public Cmdsphere() {
+			super("build.sphere");
         }
         
         public boolean execute(String[] args) {
-        	player.sendMessage("waaaa");	
-        	slices = new Stack();
-				b = mye.getPoint(0, player);
+			player.sendMessage("waaaa");    
+            slices = new Stack();
+			b = mye.getPoint(0, player);
+			idO = 0;
+
+			if (b == null)
+				player.sendMessage("seteame el punto, o no te construyo la esfera!");
+			else{
+				player.sendMessage("esfera");
+				if ((args.length != 3 ) && (args.length != 4 ))
+					player.sendMessage("El comando para crear esferas tiene más parámetros!!!");
 				
-				if (b == null)
-					player.sendMessage("seteame el punto, o no te construyo la esfera!");
-				else{
-					player.sendMessage("esfera");
-					r = Integer.parseInt(args[0]);
-					c = Integer.parseInt(args[1]);
-					id = 20;
+				r = Integer.parseInt(args[0]);
+				idS = Integer.parseInt(args[1]);
+				idI = Integer.parseInt(args[2]);
+				
+				if (args[3] == null)
+					c = 1
+				else
+					c = Integer.parseInt(args[3]);
 					
-					if (c==0)
-						mCubes = Math.ceil(r - 1 / 2) * 2 - 1;
-					else
-						mCubes = Math.ceil(r) * 2;
-					
+				if (c == 0)
+					mCubes = Math.ceil(r - 1 / 2) * 2 - 1;
+				else
+					mCubes = Math.ceil(r) * 2;
+				
+				if ((int)(b.getY() + mCubes) <= 128){
 					for (double z = -mCubes / 2, i=0; z <= mCubes / 2; z++, i++)
 						slices.push(new Double(Math.sqrt(Math.pow(r,2) - Math.pow(z,2))));
-					
+				
 					while (slices.size() > 0){
-						radius = ((Double) (slices.pop())).doubleValue();  			
+						radius = ((Double) (slices.pop())).doubleValue();                       
 						for (double y = -mCubes / 2, cY = 1; y <= mCubes / 2; y++, cY++){
 							for (double x = -mCubes / 2, cX = 1; x <= mCubes / 2; x++, cX++){
 								if ((Math.sqrt(Math.pow(y,2) + Math.pow(x,2))) > radius){
-									player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(0);
+									player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(0); //out
 								}else{
-									player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(id);
-								}
+									if (c==1){
+										if (((slices.size() == 1)||(slices.size() == (cSlices-1)))){
+											player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(idS); //shell
+										}else{
+											if ((Math.sqrt(Math.pow(y,2) + Math.pow(x,2))) > (radius - 2)){
+												player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(idS); //shell
+											}else{
+												player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(0); //out
+											}
+										}
+									}else{
+										if ((slices.size() == 0)||(slices.size() == cSlices-1)||(slices.size() == 1)||(slices.size() == (cSlices-2))){
+											player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(idS); //shell
+										}else{
+											if ((Math.sqrt(Math.pow(y,2) + Math.pow(x,2))) > (radius - 2)){
+												player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(idS); //shell
+											}else{
+												player.getWorld().getBlockAt((int)(b.getX()+cX),(int)(b.getZ()+slices.size()),(int)(b.getY()+cY)).setTypeId(idI); //in
+											}
+										}
+									}
+								}							
 							}
 						}
-					}				
-				}
-				return true;
-        }
+					}
+				}else
+					player.sendMessage("no da la altura del mapa para hacer una esfera con radio " + r);
+			}
+			return true;
+	}
 }
